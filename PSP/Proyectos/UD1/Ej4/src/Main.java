00da6dec523c4866d8633ff30e6aa3a75fee488a
Hugo Tarin Gonzalez <taringonzalezhugo@gmail.com>
2025-10-07 23:58:15 +0200

    Avanzar Ej4 PSP

--- PSP/Proyectos/UD1/Ej4/src/Main.java
+++ PSP/Proyectos/UD1/Ej4/src/Main.java
@@ -1,18 +1,51 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<String> comandos = new ArrayList<>(Arrays.asList("cmd.exe","/c","dir"));
         ProcessBuilder pb =  new ProcessBuilder(comandos);
         try {
+            // DEFECTO
             Process p = pb.start();
             System.out.printf("Directorio por defecto: %s%n",pb.directory());
-            System.out.printf("Environment por defecto: %s%n",pb.environment());
+            System.out.printf("Environment por defecto:%n");
+            System.out.printf("------------------------%n");
+            Map<String,String> env = pb.environment();
+            for (String key : env.keySet()) {
+                System.out.printf("| %s= %s%n",key,env.get(key));
+            }
+            System.out.printf("------------------------%n");
+
+            p.destroy();
+
+            // PRUEBA 1
+            p = pb.start();
+            File dir = new File("C:\\Users\\usuario\\Documents\\2DAM_Hugo\\PSP");
+            pb.directory(dir);
+            System.out.printf("--- PRUEBA 1 ---%n%n");
+            System.out.printf("Directorio: %s%n%n",pb.directory());
+            System.out.printf("------------------------%n");
+
+            p.destroy();
+
+            // PRUEBA 2
+            p = pb.start();
+            System.out.printf("--- PRUEBA 2 ---%n%n");
+            System.out.printf("Directorio: %s%n%n",pb.directory());
+            System.out.printf("------------------------%n");
+
+            p.destroy();
+
+            // PRUEBA 3
+            p = pb.start();
+            System.out.printf("--- PRUEBA 3 ---%n%n");
+            System.out.printf("Directorio: %s%n%n",pb.directory());
+            System.out.printf("------------------------%n");
 
             p.destroy();
         } catch (IOException e) {
